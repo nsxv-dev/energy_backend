6ad8d6aad71c23be95fecdd7a4f189ab09de8a6f
nsxv <189486048+nsxv-dev@users.noreply.github.com>
2025-12-15 14:06:48 +0100

    config

--- demo/src/test/java/com/example/demo/service/EnergyServiceTest.java
+++ demo/src/test/java/com/example/demo/service/EnergyServiceTest.java
@@ -4,19 +4,16 @@ import com.example.demo.dto.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
+
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
