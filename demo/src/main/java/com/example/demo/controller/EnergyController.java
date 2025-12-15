d14746aa22cab9f56fe3ce9b1e96088eca15825d
nsxv <189486048+nsxv-dev@users.noreply.github.com>
2025-12-15 15:02:02 +0100

    config

--- demo/src/main/java/com/example/demo/controller/EnergyController.java
+++ demo/src/main/java/com/example/demo/controller/EnergyController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class EnergyController {
 
     private final EnergyService energyService;
